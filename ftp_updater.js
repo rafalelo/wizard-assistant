2360b3f60ea20089139808ce798455c340aa1ff6
gerwant <damian.gorzelanczyk@gmail.com>
2020-08-21 13:21:52 +0200

    :sparkles: Complete FTP strategy overhaul.

---
+++ ftp_updater.js
@@ -0,0 +1,193 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const FTPClient = require('ftp');
+const _ = require('underscore')
+
+const client = new FTPClient();
+const excluded_dirs = ['.', '..', '.ftpquota']
+const dpath = path.join(__dirname, "devices")
+
+let devices = []
+
+let device_sensor = [] 
+
+function update_hexfiles() {
+
+    let conf = device_sensor.pop()
+
+    client.listSafe(`/${conf.device}/${conf.sensor}`, false, (err, listing) => {
+        let firmwares = _.chain(listing)
+                        .filter( (el) => { return el.type == '-'})
+                        .map((el) => { return el.name })
+                        .value()
+
+        //console.log(`Firmwares for ${conf.device}/${conf.sensor}: ${firmwares}`)
+        
+        let current_items = fs.readdirSync(path.join(dpath, conf.device, conf.sensor), err => { console.error(err)})
+        for (item of current_items) {
+            if (fs.existsSync(path.join(dpath, conf.device, conf.sensor, item))){//&&fs.statSync(path.join(dpath, device, item)).isDirectory())){
+                fs.rmdirSync(path.join(dpath, conf.device, conf.sensor, item), {recursive: true})
+            }
+        }   
+
+        _.each(firmwares, (el, k, idx) => {
+            try{
+                if (!fs.existsSync(path.join(dpath, conf.device, conf.sensor, el))){
+                    fs.writeFile(path.join(dpath, conf.device, conf.sensor, el),'', (err)=>{
+                        if (err) {console.log(err)}
+                    })
+                }
+            } catch (err){
+                console.error(err)
+            }
+        })
+
+        if (device_sensor.length >0 ) {
+            update_hexfiles();
+        }
+
+    })
+    
+    return true;
+}
+
+function update_sensors(){
+
+    device = devices.pop()
+
+    client.listSafe(`/${device}`, false, (err, listing) => {
+
+        let sensors = _.chain(listing)
+            .filter((el) => { return el.type == 'd' && !excluded_dirs.includes(el.name) })
+            .map((el) => { return el.name })
+            .value()
+
+        let confs = _.chain(listing)
+            .filter((el)=> {return el.type == '-'})
+            .map((el)=> { return el.name})
+            .value()
+
+        let current_items = fs.readdirSync(path.join(dpath, device), err => { console.error(err)})
+        for (item of current_items) {
+            if (fs.existsSync(path.join(dpath, device, item))){//&&fs.statSync(path.join(dpath, device, item)).isDirectory())){
+                fs.rmdirSync(path.join(dpath, device, item), {recursive: true})
+            }
+        }
+
+        //for (item of current_items) {
+        //    fs.unlink(path.join(dpath, device, item), err => {
+        //        if (err) console.error(err);
+        //    })
+        //}
+
+        _.each(listing, (el, k, idx) => {
+            try{
+                if (!fs.existsSync(path.join(dpath, device, el.name))){
+                    if (el.type == "d"){
+                        fs.mkdirSync(path.join(dpath, device, el.name))
+                        device_sensor.push({device: device, sensor: el.name})
+                    } else {
+                        fs.writeFile(path.join(dpath, device, el.name),'', (err)=>{ if (err) {console.log(err)} }); 
+                    }
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        })
+        
+        if (devices.length>0){
+
+            update_sensors();
+
+        } else {
+            
+            update_hexfiles();
+        }
+
+    })
+
+}
+
+function update_devices(new_devices){
+    console.log("Updating devices")
+    let current_devices = fs.readdirSync(dpath)
+    _.each(new_devices, (el, k, idx) => {
+        if (!current_devices.includes(el)){
+            try {
+                if (!fs.existsSync(path.join(dpath, el))) {
+                    fs.mkdirSync(path.join(dpath, el))
+                }
+            } catch (err) {
+                console.log(err) // TODO: Change to logging to file
+            }
+        }
+    })
+    _.each(current_devices, (el, k, idx) => {
+        if (!new_devices.includes(el)){
+            fs.rmdir(path.join(dpath, el), {recursive: true }, (err) => {
+                if (err){
+                    console.log("Directory remove error: ", err) // TODO: Change to logging to file
+                }
+            })
+        }
+    })
+
+    // So far we have to be sure that folders in device/ are up to date
+
+    devices = new_devices;
+    //new_devices.forEach( (device) => {
+    //    update_sensors(device)
+    //})
+    update_sensors();
+}
+
+client.on('ready', () => {
+    client.listSafe('/', false, (err, listing) => {
+        devices = _.filter(listing, (element) => {
+            return element.type == 'd' && !excluded_dirs.includes(element.name); // d because type d means it's a directory. And we don't want parent and current directory symlinks
+        })
+        devices = _.map(devices, (el) => {
+            return el.name
+        })
+        update_devices(devices);
+    })
+})
+
+client.on('error', (error) => {
+    console.error(error); // TODO: change console logging to logging to file
+})
+
+router.get('/', async (req, res) => {
+
+    //new ftpdump({
+    //    host: process.env.FTP_HOST,
+    //    port: 21,
+    //    user: process.env.FTP_USER,
+    //    password: process.env.FTP_PASSWORD,
+    //    root: "/"
+    //}, dpath, (err) => {
+    //    if (err) return console.log(err);
+    //});
+
+
+    //res.sendStatus(200)
+    try {
+        client.connect({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD,
+            secure: false
+        })
+        
+        res.sendStatus(200)
+    } catch(err) {
+        // Should terminate window and return to welcome window
+        // with appropriate error message
+        console.log("FTP ERROR: ", err)
+        res.sendStatus(500)
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {resolve} = require('path');
 const FTPClient = require('ftp');
 const app = express()
+const path = require('path')
+const fs = require('fs')
 const _ = require('underscore');
 
 if (process.env.NODE_ENV) {
@@ -12,145 +14,78 @@ if (process.env.NODE_ENV) {
     console.log('Development .env file loaded.')
 }
 
-const client = new FTPClient();
-const excluded_dirs = ['.', '..']
-let connected;
-
-client.on('ready', () => {
-    connected = true;
-})
-
-client.on('end', () => {
-    connected = false;
-    connectFTP();
-})
-
-client.on('error', (error) => {
-    console.error(error);
-})
-
-
-function connectFTP(){
-    try {
-        client.connect({
-            host: process.env.FTP_HOST,
-            user: process.env.FTP_USER,
-            password: process.env.FTP_PASSWORD,
-            secure: false
-        })
-        
-        return true
-    } catch(err) {
-        // Should terminate window and return to welcome window
-        // with appropriate error message
-        console.log("FTP ERROR: ", err)
-        return false
-    }
-}
-
-function reconnectFTP(){
-    
-    client.end();
-    return
-}
-
-connectFTP();
-
-setInterval(reconnectFTP, 180000) // Reconnect interval, every 180000ms=3min
+const dpath = path.join(__dirname, "devices")
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+app.use('/update', require('./ftp_updater'))
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendStatus(200)
+})
+
 app.get('/dev/:device/:sensor', (req, res)=>{
-    let count = 0
-    while(!connected && count < 5)
-    {
-        setTimeout(1000)
-        count ++
-    }
+    
     let device = req.params.device
     let sensor = req.params.sensor
     let processor
     let baud
 
- 
+    let firmwares = fs.readdirSync(path.join(dpath, device, sensor), err => {console.error(err)})
 
-    client.listSafe(`/${device}/${sensor}`, false, (err, listing) => {
-        console.log(listing)
-        listing = _.filter(listing, (element)=>{
-            return element.type == '-'; // 2 because type 2 means it's a directory
-        })
-        listing = _.map(listing, (element) => {
-            return element.name;
-        })
-        console.log(listing)
-        res.send({"devices": listing})
-    })
+    res.send({"devices": firmwares})
+
+    //client.listSafe(`/${device}/${sensor}`, false, (err, listing) => {
+    //    console.log(listing)
+    //    listing = _.filter(listing, (element)=>{
+    //        return element.type == '-'; // 2 because type 2 means it's a directory
+    //    })
+    //    listing = _.map(listing, (element) => {
+    //        return element.name;
+    //    })
+    //    console.log(listing)
+    //    res.send({"devices": listing})
+    //})
     
     
 })
 app.get('/devices', (req, res) => {
-    let count = 0
-    while(!connected && count < 5)
-    {
-        setTimeout(1000)
-        count ++
-    }
-    if (!connected){
-        console.error('No ftp connection')
-        res.status(404).send('No ftp connection')
-        reconnectFTP();
-        return;
-    }
-    client.listSafe('/', false, (err, listing) => {
-        listing = _.filter(listing, (element)=>{
-            return element.type == 'd' && !excluded_dirs.includes(element.name); // d because type d means it's a directory. And we don't want parent and current directory symlinks
-        })
-        listing = _.map(listing, (element) => {
-            return element.name;
-        })
-        res.send({"devices": listing})
-    })
+    
+    let devices = fs.readdirSync(dpath); // TODO: Ensure all returned items are directories
+
+    res.send({"devices": devices})
+    
 })
 
 app.get('/:device', (req, res)=>{
     let count = 0
-    while(!connected && count < 5)
-    {
-        setTimeout(1000)
-        count ++
-    }
     
     let device = req.params.device
     let processor
     let baud
-    client.listSafe(`/${device}`,false, (err, listing) =>{
-        let procfile = _.find(listing, (el) => { return el.name.includes('.processor')})// Look for .processor file
-        let baudfile = _.find(listing, (el) => { return el.name.includes('.baudrate')}) // Look for .baudrate file
 
+    let listing = fs.readdirSync(path.join(dpath, device));
+
+    let procfile = _.find(listing, (el) => { return el.includes('.processor')})// Look for .processor file
+    let baudfile = _.find(listing, (el) => { return el.includes('.baudrate')}) // Look for .baudrate file
+
+    let sensors = _.filter(listing, (el)=> { return fs.lstatSync(path.join(dpath, device, el)).isDirectory()})
     
-        if (procfile && baudfile){
-            processor = procfile.name.split('.')[0]
-            baud = baudfile.name.split('.')[0]
-            let lst = _.filter(listing, (el) => { //Look for all directories meaning sensors
-                return el.type == 'd' && !excluded_dirs.includes(el.name);
-            })
-            lst = _.map(lst, (el) => { // Select only names of them
-                return el.name;
-            })
-            res.send({"baudrate": baud, "processor": processor, "sensors": lst})
-        } else { // No .processor file means something went wrong and flash can't be performed
-            res.send('')
-        }
+    if (procfile && baudfile){
+        processor = procfile.split('.')[0]
+        baud = baudfile.split('.')[0]
         
-    })
-
-    
+        res.send({"baudrate": baud, "processor": processor, "sensors": sensors})
+    } else { // No .processor file means something went wrong and flash can't be performed
+        res.send('') // TODO: should be replaced with status 404 or 500
+    }
+        
 })
 
 app.get('/*', function(req, res) {
      
-        res.status(404).send("Not found")
+    res.status(404).send("Not found")
 
 })
 
